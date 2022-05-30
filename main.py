dfa75cd2ef9898f5abf4fff8e3ce48113d8c1222
AsperTheDog <guillermo.munnozr@estudiante.uam.es>
2022-05-30 03:04:08 +0200

    Complete overhaul of the system
    The program now has a worker thread that rasterizes frames into a buffer, the system also now calculates times correctly and recovers from lag when possible. Added progress bar, proper aspect ratio and videos are now centered

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import math
 import os.path
 import time
 import sys
 import configparser
+
+from Lib import queue
+import threading
 
 import numpy as np
 import cv2
@@ -12,47 +16,133 @@ path = ""
 filename = ""
 
 levelPal = ' .,:;<iIEX%@'
+showBar = True
 
+buffer = queue.Queue()
+finished = False
+fps = 30
+b = threading.Barrier(2)
+size = (50, 50)
+res = (0, 0)
+padding = (0, 0)
 
-def main(sc):
-    x, y = sc.getmaxyx() if sc else (50, 50)
-    size = (min(x, y) - 1, min(x, y) - 1)
+totalFrames = 0
+rasterFrames = 0
+videoFrames = 0
 
-    vid = cv2.VideoCapture(path)
-    fps = round(vid.get(cv2.CAP_PROP_FPS))
+def feeder(vid):
+    global buffer, finished, size, b, rasterFrames
 
-    npShape = (size[0], size[1], 1)
+    b.wait()
 
-    tm = time.perf_counter()
+    trueWidth = size[0] * 2 + 1 + padding[0]
+    frameData = [' '] * (trueWidth * (size[1] + padding[1]))
+
+    for y in range(1, size[1]):
+        frameData[(y * trueWidth) - 1] = "\n"
+
     while True:
         ret, frame = vid.read()
         if ret:
             img = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), size, interpolation=cv2.INTER_AREA).astype(int)
 
-            for y, x, _ in np.ndindex(npShape):
-                sc.addstr(y, x * 2, levelPal[int(img[y, x] * len(levelPal) // 256)])
+            for y, x in np.ndindex(img.shape):
+                pos = y * trueWidth + (x * 2) + padding[0]
+                newChar = levelPal[int(img[y, x] * len(levelPal) >> 8)]
+                if newChar != frameData[pos]:
+                    frameData[pos] = frameData[pos + 1] = newChar
 
-            sc.refresh()
-
-            tm = time.perf_counter() - tm
-            if tm < (1 / fps):
-                time.sleep((1 / fps) - tm)
-            tm = time.perf_counter()
+            lastFrame = "\n" * padding[1] + "".join(frameData)
+            buffer.put(lastFrame)
+            rasterFrames += 1
         else:
-            break
-    return
+            return
+
+
+def renderer(sc):
+    global buffer, fps, size, b, res, videoFrames, rasterFrames, totalFrames, showBar, padding
+
+    curses.curs_set(0)
+
+    ar = res[1] / res[0]
+    y, x = sc.getmaxyx() if sc else (50, 50)
+
+    y -= 1
+    if showBar:
+        y -= 2
+
+    xr, yr = math.floor(y / ar), math.floor(x * ar)
+    size = (min(xr, x), min(yr, y))
+
+    padding = (math.floor((x - (size[0] * 2)) // 2), math.floor((y - size[1]) // 2))
+
+    b.wait()
+
+    frRatio = (x - 2) / totalFrames
+    frameTime = (1 / fps)
+    totalTime = 0
+    tm = time.perf_counter()
+    while True:
+        if finished:
+            if buffer.empty():
+                return
+            nextFrame = buffer.get(block=False)
+        else:
+            nextFrame = buffer.get()
+        sc.addstr(0, 0, nextFrame)
+
+        if showBar:
+            adjVideo = math.ceil(videoFrames * frRatio)
+            adjRaster = min(math.ceil(buffer.qsize() * frRatio), x - 2 - adjVideo)
+            sc.addstr(y + 1, 0, "|" + ("-" * (x - 2)) + "|")
+            sc.addstr(y + 1, 1, ("#" * adjVideo) + ("=" * adjRaster))
+
+        sc.refresh()
+
+        totalTime += time.perf_counter() - tm
+        tm = time.perf_counter()
+        target = frameTime * videoFrames
+        if totalTime < target:
+            time.sleep(target - totalTime)
+        videoFrames += 1
+
+
+def multiMain():
+    global fps, res, totalFrames
+
+    vid = cv2.VideoCapture(path)
+    fps = round(vid.get(cv2.CAP_PROP_FPS))
+    totalFrames = round(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    res = (width, height)
+
+    feed = threading.Thread(target=feeder, args=[vid])
+    feed.start()
+    curses.wrapper(renderer)
 
 
 def configure():
-    global levelPal
+    global levelPal, buffer, showBar
     config = configparser.ConfigParser()
     config.read('config.cfg')
     try:
         levelPal = " " + config["VIDEO"]["characterPalette"]
-        levelPal = [ch * 2 for ch in levelPal]
+        try:
+            if "maxBufferSize" in config["VIDEO"] and int(config["VIDEO"]["maxBufferSize"]) > 0:
+                buffer = queue.Queue(int(config["VIDEO"]["maxBufferSize"]))
+        except ValueError:
+            pass
+        showBar = config["VIDEO"]["showProgressBar"] == "True"
     except KeyError:
         config = configparser.ConfigParser()
-        config["VIDEO"] = {"characterPalette": levelPal.replace("%", "%%")}
+        config["VIDEO"] = {
+            "characterPalette": levelPal.replace("%", "%%"),
+            "maxBufferSize": 0,
+            "showProgressBar": True
+        }
         levelPal = [ch * 2 for ch in levelPal]
         with open("config.cfg", "w") as cfgFile:
             config.write(cfgFile)
@@ -83,9 +173,9 @@ if __name__ == "__main__":
         sys.exit(1)
 
     try:
-        curses.wrapper(main)
+        multiMain()
     except curses.error:
-        print("Error, the console is too small, please expand the window!!")
+        print("An error displaying the screen occured, please restart the program")
     except Exception:
         print("Error, the file could not be read")
     input("press enter to exit...")
